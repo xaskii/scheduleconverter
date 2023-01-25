5ba7e73912182c9f2fb891b9ff7cf577ffec81fc
Xaskii <21989858+Xaskii@users.noreply.github.com>
2023-01-25 01:22:08 -0500

    implemented repeating rules, running into string escape issue

--- converter.py
+++ converter.py
@@ -1,6 +1,11 @@
 import re
-import icalendar
-from pprint import pprint
+import os
+import pytz
+from icalendar import Calendar, Event, vDatetime, vDDDTypes
+from datetime import datetime, timedelta, date, time
+from dateutil.parser import parse
+
+from helper import objprint, iCalToString, getNextWeekday, daysToRRULE
 
 
 class Course:
@@ -15,8 +20,24 @@ courseList = []
 rx_dict = {
     'course': re.compile(r'(?P<name>.+) - (?P<code>.+) - (?P<section>\w+)\n'),
     'instructor': re.compile(r'Assigned Instructor:\s(?P<instructor>.+)\n'),
-    'classInfo': re.compile(r'Class\t(?P<startTime>\d:\d{2} \w{2}) - (?P<endTime>\d:\d{2} \w{2})\t(?P<days>\w+)\t(?P<building>.*)\t(?P<startDate>.*)\ - (?P<endDate>.*)\t(?P<type>.*)\t.*\n')
+    'classInfo': re.compile(r'Class\t(?P<startTime>\d{1,2}:\d{2} \w{2}) - (?P<endTime>\d{1,2}:\d{2} \w{2})\t(?P<days>\w+)\t(?P<building>.*)\t(?P<startDate>.*)\ - (?P<endDate>.*)\t(?P<type>.*)\t.*\n')
 }
+
+timezone = pytz.timezone("US/Eastern")
+
+weekdays = {
+    'Monday': 0,
+    'Tuesday': 1,
+    'Wednesday': 2,
+    'Thursday': 3,
+    'Friday': 4,
+    'Saturday': 5,
+    'Sunday': 6,
+}
+
+
+def onDay(dt, day):
+    return dt + timedelta(days=(day-dt.weekday()) % 7)
 
 
 def _parse_line(line):
@@ -30,11 +51,11 @@ def _parse_line(line):
 def parse_file(path):
     data = []
     with open(path, 'r') as file:
-        line = file.readline()
-        while line:
+        for line in file:
             key, match = _parse_line(line)
 
             if key == 'course':
+                newCourse = None
                 name = match.group('name')
                 code = match.group('code')
                 section = match.group('section')
@@ -44,24 +65,75 @@ def parse_file(path):
                 newCourse.instructor = match.group('instructor')
 
             if key == 'classInfo':
-                # TODO: parse into date objects?
+                newCourse.location = match.group('building')
+                # TODO: parse into date objects? done haha
                 newCourse.type = match.group('type')
                 newCourse.times = {
-                    'start': match.group('startTime'),
-                    'end': match.group('endTime')
+                    'start': parse(match.group('startTime')),
+                    'end': parse(match.group('endTime'))
                 }
                 newCourse.dates = {
-                    'start': match.group('startDate'),
-                    'end': match.group('endDate')
+                    'start': parse(match.group('startDate')),
+                    'end': parse(match.group('endDate'))
                 }
+                # TODO: figure out starting days
                 newCourse.days = match.group('days')
                 courseList.append(newCourse)
 
-            line = file.readline()
     pass
 
 
-parse_file('paste.txt')
+parse_file('paste.test.txt')
+# parse_file('course.test.txt')
+
+
+cal = Calendar()
+rruleTemplate = []
 
 for course in courseList:
-    pprint(course.__dict__, indent=2)
+    event = Event()
+    event['summary'] = f'{course.code}-{course.section}'
+    event['description'] = course.name
+    event['location'] = course.location
+
+    untilDate = vDDDTypes(course.dates['end']).to_ical().decode('utf-8')
+    startDate = vDDDTypes(datetime.combine(
+        getNextWeekday(course.dates['start'].date(), course.days[0]), course.times['start'].time()))
+
+    event['dtstart'] = startDate
+    event['duration'] = vDDDTypes(course.times['end'] - course.times['start'])
+    event['rrule'] = [
+        'FREQ=WEEKLY',
+        'COUNT=12',
+        f'UNTIL={untilDate}',
+        # ESCAPES THE COMMAS???? WHAT DO I DO LOL???
+        # ?????????????????????????????????????
+        f'BYDAY={daysToRRULE(course.days)}'
+    ]
+
+    cal.add_component(event)
+
+    # break  # making first event
+
+# print(iCalToString(cal))
+
+# write to file
+directory = os.getcwd()
+f = open(os.path.join(directory, 'example.ics'), 'wb')
+f.write(cal.to_ical())
+f.close()
+
+
+print("CALENDAR SUMMARY:\n")
+e = open('example.ics', 'rb')
+ecal = Calendar.from_ical(e.read())
+for component in ecal.walk():
+    if component.name == "VEVENT":
+        print(component.get("summary"))
+        print(component.get("description"))
+        print(component.get("organizer"))
+        print(component.get("location"))
+        print(component.decoded("dtstart"))
+        print(component.decoded("duration"))
+        print()
+e.close()
---
+++ helper.py
@@ -0,0 +1,40 @@
+from pprint import pprint
+from datetime import datetime, timedelta
+
+
+def objprint(obj):
+    pprint(obj.__dict__, indent=2)
+
+
+def iCalToString(cal):
+    return cal.to_ical().decode("utf-8").replace('\r\n', '\n').strip()
+
+
+def onDay(dt, day):
+    return dt + timedelta(days=(day-dt.weekday()) % 7)
+
+
+def getNextWeekday(dt, dayChar):
+    lookup = {
+        'M': 0,
+        'T': 1,
+        'W': 2,
+        'R': 3,
+        'F': 4,
+    }
+    return onDay(dt, lookup[dayChar])
+
+
+def daysToRRULE(days):
+    codes = []
+    lookup = {
+        'M': 'MO',
+        'T': 'TU',
+        'W': 'WE',
+        'R': 'TH',
+        'F': 'FR',
+    }
+    for day in days:
+        codes.append(lookup[day])
+
+    return ','.join(codes)
