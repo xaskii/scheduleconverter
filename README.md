8aba36df34fc8c58d43a3b5c16cc19b8fb551fdb
Xavi <21989858+xaskii@users.noreply.github.com>
2024-09-04 10:12:23 -0400

    pyproject.toml + uv will prob switch back to requirements.txt later

--- README.md
+++ README.md
@@ -2,27 +2,19 @@
 
 Converts "Detailed Schedule" page from Carleton Central to an **icalendar** file that you can import
 
-## Setup
-
-1. Create a [virtual environment](https://docs.python.org/3/library/venv.html)
-   (optional)
-
-2. Install dependencies using [pip](https://pip.pypa.io/en/stable/)
-
+## Usage
 ```bash
-python3 pip install -r requirements.txt
+uv sync
+uv run scheduleexporter/converter.py \<path of paste here\>
 ```
 
-## Usage
-
-TBD, have to change stuff to not use a file input. Right now, it parses "paste.text.txt"
 
 ## TODO
 
 - [x] Find iCalendar file library
 - [x] Parse out paste from Detailed Schedule page
-- [x] Solve reoccuring event problem (finally fixed holy)
-- [ ] Make an interface for the user to paste in their infromation
+- [x] Solve recurring event problem (finally fixed holy)
+- [ ] Make an interface for the user to paste in their information
 - [ ] Change into an API then make a front-end
 
 ## Notes
--- scheduleexporter/converter.py
+++ scheduleexporter/converter.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from dateutil.parser import parse
 from icalendar import Calendar, Event, vDDDTypes
 
-from helper import days_to_rrule, get_next_weekday, parse_line
+from scheduleexporter.helper import days_to_rrule, get_next_weekday, parse_line
 
 
 @dataclass
@@ -62,67 +62,68 @@ def ingest_paste(filepath: Path) -> list[Course]:
 
 
 # weird shit starts here
-parser = ArgumentParser()
-parser.add_argument("path")
-parser.add_argument("-o", "--output", default="example.ics", required=False)
-args = parser.parse_args()
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("path")
+    parser.add_argument("-o", "--output", default="example.ics", required=False)
+    args = parser.parse_args()
 
-calendar_path = Path(args.path)
+    calendar_path = Path(args.path)
 
-if not calendar_path.exists():
-    print("Filepath not found, please check the path you've given.")
-    raise SystemExit(1)
+    if not calendar_path.exists():
+        print("Filepath not found, please check the path you've given.")
+        raise SystemExit(1)
 
-course_list = ingest_paste(calendar_path)
-# parse_file('course.test.txt')
+    course_list = ingest_paste(calendar_path)
+    # parse_file('course.test.txt')
 
+    cal = Calendar()
 
-cal = Calendar()
+    for course in course_list:
+        event = Event()
+        event["summary"] = f"{course.code}-{course.section}"
+        event["description"] = course.name
+        event["location"] = course.location
 
-for course in course_list:
-    event = Event()
-    event["summary"] = f"{course.code}-{course.section}"
-    event["description"] = course.name
-    event["location"] = course.location
-
-    # untilDate = vDDDTypes(course.dates['end']).to_ical().decode('utf-8')
-    untilDate = course.dates["end"]
-    startDate = vDDDTypes(
-        datetime.combine(
-            get_next_weekday(course.dates["start"].date(), course.days[0]),
-            course.times["start"].time(),
+        # untilDate = vDDDTypes(course.dates['end']).to_ical().decode('utf-8')
+        untilDate = course.dates["end"]
+        startDate = vDDDTypes(
+            datetime.combine(
+                get_next_weekday(course.dates["start"].date(), course.days[0]),
+                course.times["start"].time(),
+            )
         )
-    )
 
-    event["dtstart"] = startDate
-    event["duration"] = vDDDTypes(course.times["end"] - course.times["start"])
-    event.add(
-        "rrule",
-        {
-            "FREQ": "WEEKLY",
-            "COUNT": 12,
-            "UNTIL": untilDate,
-            "BYDAY": days_to_rrule(course.days),
-        },
-    )
-    cal.add_component(event)
+        event["dtstart"] = startDate
+        event["duration"] = vDDDTypes(course.times["end"] - course.times["start"])
+        event.add(
+            "rrule",
+            {
+                "FREQ": "WEEKLY",
+                "COUNT": 12,
+                "UNTIL": untilDate,
+                "BYDAY": days_to_rrule(course.days),
+            },
+        )
+        cal.add_component(event)
 
+    # write to file
+    f = open(os.path.join(os.getcwd(), "example.ics"), "wb")
+    f.write(cal.to_ical())
+    f.close()
 
-# write to file
-f = open(os.path.join(os.getcwd(), "example.ics"), "wb")
-f.write(cal.to_ical())
-f.close()
+    print("CALENDAR SUMMARY:\n")
+    e = open("example.ics", "rb")
+    ecal = Calendar.from_ical(e.read().decode("utf-8"))
+    for component in ecal.walk():
+        if component.name == "VEVENT":
+            print(component.get("summary"))
+            print(component.get("description"))
+            print(component.get("location"))
+            print(component.decoded("dtstart"))
+            print(component.decoded("duration"))
+            print()
+    e.close()
 
-
-print("CALENDAR SUMMARY:\n")
-e = open("example.ics", "rb")
-ecal = Calendar.from_ical(e.read().decode("utf-8"))
-for component in ecal.walk():
-    if component.name == "VEVENT":
-        print(component.get("summary"))
-        print(component.get("description"))
-        print(component.get("location"))
-        print(component.decoded("dtstart"))
-        print(component.decoded("duration"))
-        print()
-e.close()
+if __name__ == "__main__":
+    main()
