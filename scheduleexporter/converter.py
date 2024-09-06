d198fec795e5a39f3f0121d0c40a4f9c46e4e955
Xavi <21989858+xaskii@users.noreply.github.com>
2024-09-05 22:37:15 -0400

    more refactoring, I'm going to get rid of the icalendar dependency

--- scheduleexporter/converter.py
+++ scheduleexporter/converter.py
@@ -3,11 +3,15 @@ from argparse import ArgumentParser
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
 from pathlib import Path
+from typing import Dict, List, Optional
 
 from dateutil.parser import parse
 from icalendar import Calendar, Event, vDDDTypes
 
 from scheduleexporter.helper import days_to_rrule, get_next_weekday, parse_line
+
+DEFAULT_OUTPUT_FILE = "timetable.ics"
+CALENDAR_SUMMARY_FILE = "example.ics"
 
 
 @dataclass
@@ -18,59 +22,128 @@ class Course:
     instructor: str = ""
     location: str = ""
     _type: str = ""
-    times: dict[str, datetime] = field(default_factory=dict)
-    dates: dict[str, datetime] = field(default_factory=dict)
+    times: Dict[str, datetime] = field(default_factory=dict)
+    dates: Dict[str, datetime] = field(default_factory=dict)
     days: str = ""
 
 
-def ingest_paste(filepath: Path) -> list[Course]:
-    course_list: list[Course] = []
-    with filepath.open("r") as file:
-        current = None
-        for line in file:
-            key, match = parse_line(line)
-            if not match:
-                continue
+@dataclass
+class CalendarEvent:
+    summary: str
+    description: str
+    location: str
+    start_time: datetime
+    duration: timedelta
+    end_date: datetime
+    days: str
 
-            if key == "course":
-                name = match.group("name")
-                code = match.group("code")
-                section = match.group("section")
-                current = Course(name, code, section)
 
-            if key == "instructor":
-                assert type(current) is Course
-                current.instructor = match.group("instructor")
+def ingest_paste(filepath: Path) -> List[Course]:
+    course_list: List[Course] = []
+    try:
+        with filepath.open("r") as file:
+            current: Optional[Course] = None
+            for line in file:
+                key, match = parse_line(line)
+                if not match:
+                    continue
 
-            if key == "classInfo":
-                assert type(current) is Course
-                current.location = match.group("building")
-                current._type = match.group("type")
-                current.times = {
-                    "start": parse(match.group("startTime")),
-                    "end": parse(match.group("endTime")),
-                }
-                current.dates = {
-                    "start": parse(match.group("startDate")),
-                    "end": parse(match.group("endDate")),
-                }
-                current.days = match.group("days")
-                course_list.append(current)
-                current = None
+                if key == "course":
+                    name = match.group("name")
+                    code = match.group("code")
+                    section = match.group("section")
+                    current = Course(name, code, section)
+
+                elif key == "instructor" and current:
+                    current.instructor = match.group("instructor")
+
+                elif key == "classInfo" and current:
+                    current.location = match.group("building")
+                    current._type = match.group("type")
+                    current.times = {
+                        "start": parse(match.group("startTime")),
+                        "end": parse(match.group("endTime")),
+                    }
+                    current.dates = {
+                        "start": parse(match.group("startDate")),
+                        "end": parse(match.group("endDate")),
+                    }
+                    current.days = match.group("days")
+                    course_list.append(current)
+                    current = None
+
+    except IOError as e:
+        print(f"Error reading file: {e}")
+        raise
 
     return course_list
 
 
+def create_calendar_event(course: Course) -> CalendarEvent:
+    first_day = min(
+        (get_next_weekday(course.dates["start"].date(), day) for day in course.days)
+    )
+    start_time = datetime.combine(first_day, course.times["start"].time())
+    duration = course.times["end"] - course.times["start"]
+    end_date = course.dates["end"]
+
+    return CalendarEvent(
+        summary=f"{course.code}-{course.section}",
+        description=course.name,
+        location=course.location,
+        start_time=start_time,
+        duration=duration,
+        end_date=end_date,
+        days=course.days,
+    )
+
+
+def create_ical_event(cal_event: CalendarEvent) -> Event:
+    event = Event()
+    event["summary"] = cal_event.summary
+    event["description"] = cal_event.description
+    event["location"] = cal_event.location
+    event["dtstart"] = vDDDTypes(cal_event.start_time)
+    event["duration"] = vDDDTypes(cal_event.duration)
+    event.add(
+        "rrule",
+        {
+            "FREQ": "WEEKLY",
+            "UNTIL": cal_event.end_date + timedelta(days=1),
+            "BYDAY": days_to_rrule(cal_event.days),
+        },
+    )
+    return event
+
+
+def write_calendar(cal: Calendar, filepath: str):
+    with open(filepath, "wb") as f:
+        f.write(cal.to_ical())
+
+
+def print_calendar_summary(filepath: str):
+    print("CALENDAR SUMMARY:\n")
+    with open(filepath, "rb") as f:
+        ecal = Calendar.from_ical(f.read().decode("utf-8"))
+        for component in ecal.walk():
+            if component.name == "VEVENT":
+                print(f"Summary: {component.get('summary')}")
+                print(f"Description: {component.get('description')}")
+                print(f"Location: {component.get('location')}")
+                print(f"Start time: {component.decoded('dtstart')}")
+                print(f"Duration: {component.decoded('duration')}")
+                print()
+
+
 def main():
     parser = ArgumentParser(
-        description="""Converts schedule paste into an icalendar file for import into the calendar application of your choice"""
+        description="Converts schedule paste into an icalendar file for import into the calendar application of your choice"
     )
     parser.add_argument("input", help="Path to paste containing default schedule")
     parser.add_argument(
         "-o",
         "--output_file",
-        default="timetable.ics",
-        required=False,
+        default=DEFAULT_OUTPUT_FILE,
         help="Output file path",
     )
     args = parser.parse_args()
@@ -79,60 +152,27 @@ def main():
 
     if not calendar_path.exists():
         print("Filepath not found, please check the path you've given.")
-        raise SystemExit(1)
+        return 1
 
-    course_list = ingest_paste(calendar_path)
+    try:
+        course_list = ingest_paste(calendar_path)
+    except Exception as e:
+        print(f"Error ingesting paste: {e}")
+        return 1
 
     cal = Calendar()
 
     for course in course_list:
-        event = Event()
-        event["summary"] = f"{course.code}-{course.section}"
-        event["description"] = course.name
-        event["location"] = course.location
+        cal_event = create_calendar_event(course)
+        ical_event = create_ical_event(cal_event)
+        cal.add_component(ical_event)
 
-        first_day = min(
-            (get_next_weekday(course.dates["start"].date(), day) for day in course.days)
-        )
+    output_path = os.path.join(os.getcwd(), CALENDAR_SUMMARY_FILE)
+    write_calendar(cal, output_path)
+    print_calendar_summary(output_path)
 
-        event["dtstart"] = vDDDTypes(
-            datetime.combine(
-                first_day,
-                course.times["start"].time(),
-            )
-        )
-
-        event["duration"] = vDDDTypes(course.times["end"] - course.times["start"])
-
-        end_date = course.dates["end"]
-        event.add(
-            "rrule",
-            {
-                "FREQ": "WEEKLY",
-                "UNTIL": end_date + timedelta(days=1),
-                "BYDAY": days_to_rrule(course.days),
-            },
-        )
-        cal.add_component(event)
-
-    f = open(os.path.join(os.getcwd(), "example.ics"), "wb")
-    f.write(cal.to_ical())
-    f.close()
-
-    print("CALENDAR SUMMARY:\n")
-    e = open("example.ics", "rb")
-    ecal = Calendar.from_ical(e.read().decode("utf-8"))
-    for component in ecal.walk():
-        if component.name == "VEVENT":
-            print(component.get("summary"))
-            print(component.get("description"))
-            print(component.get("location"))
-            print(component.decoded("dtstart"))
-            print(component.decoded("duration"))
-            # print(component.decoded("rrule"))
-            print()
-    e.close()
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    exit(main())
